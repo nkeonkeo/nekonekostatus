713f3c4e128023b4d0da85d76a1901a98668ed8a
mslxi <56199297+mslxi@users.noreply.github.com>
2022-04-07 18:47:47 +0800

    1

--- modules/servers/func.js
+++ modules/servers/func.js
@@ -1,27 +1,7 @@
 const ssh=require("../../ssh");
 async function initServer(server,neko_status_url){
     var sh=
-`#!/bin/bash
-if [[ "$(command -v wget)" ]]
-them echo ""
-else
-apt update -y >>/dev/null 2>&1
-apt install wget -y >>/dev/null 2>&1
-fi
-CPU=$(uname -m)
-if [[ "$CPU" == "aarch64" ]]
-then
-  cpu=arm64
-elif [[ "$CPU" == "arm" ]]
-then
-  cpu=arm7
-elif [[ "$CPU" == "x86_64" ]]
-then
-  cpu=amd64
-else
-exit 1
-fi
-wget https://github.com/nkeonkeo/nekonekostatus/releases/download/v0.1/neko-status_linux_${cpu} -O /usr/bin/neko-status && chmod +x /usr/bin/neko-status
+`bash ./install.sh
 systemctl stop nekonekostatus
 mkdir /etc/neko-status/
 echo "key: ${server.data.api.key}
