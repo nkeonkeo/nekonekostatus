4781020fae2d863544863ca18ddbecb533d601de
mslxi <56199297+mslxi@users.noreply.github.com>
2022-04-22 06:36:01 +0800

    fix

--- modules/servers/install.sh
+++ modules/servers/install.sh
@@ -1,47 +1,41 @@
 #!/bin/bash
-if [[ ! "$(command -v wget)" ]]
-then a=apt
+if [[ ! "$(command -v wget)" ]];then
+  a=apt
 cent=$(cat /etc/redhat-release 2>/dev/null)
-if [[ $(echo $cent |grep -i -E 'centos') != "" ]]
-then a=yum;
+if [[ $(echo $cent |grep -i -E 'centos') != "" ]];then
+  a=yum
 fi
 
-if ! [ -x "$(command -v curl)" ]; then
+if ! [ -x "$(command -v curl)" ];then
   ${a} update -y >>/dev/null 2>&1
   ${a} install curl -y >>/dev/null 2>&1
 fi
 
-if ! [ -x "$(command -v wget)" ]; then
+if ! [ -x "$(command -v wget)" ];then
   ${a} update -y >>/dev/null 2>&1
   ${a} install wget -y >>/dev/null 2>&1
 fi
 
-systemctl stop nekonekostatus
 [[ -f /usr/bin/neko-status ]] && rm -rf /usr/bin/neko-status && systemctl stop nekonekostatus
 [[ ! -d /etc/neko-status/ ]] && mkdir /etc/neko-status/
 
 CPU=$(uname -m)
-if [[ "$CPU" == "aarch64" ]]
-then
+if [[ "$CPU" == "aarch64" ]];then
   cpu=arm64
-elif [[ "$CPU" == "arm" ]]
-then
+elif [[ "$CPU" == "arm" ]];then
   cpu=arm7
-elif [[ "$CPU" == "x86_64" ]]
-then
+elif [[ "$CPU" == "x86_64" ]];then
   cpu=amd64
-elif [[ "$CPU" == "s390x" ]]
-then
-    cpu=s390x
+elif [[ "$CPU" == "s390x" ]];then
+  cpu=s390x
 else
-exit 1
+  exit 1
 fi
 
-
-if [[ $(curl -m 10 -s https://ipapi.co/json | grep 'China') != "" ]]; then
-    URL="dn-dao-github-mirror.daocloud.io"
+if [[ $(curl -m 10 -s https://ipapi.co/json | grep 'China') != "" ]];then
+  URL="dn-dao-github-mirror.daocloud.io"
 else
-    URL="github.com"
+  URL="github.com"
 fi
 
 wget https://${URL}/nkeonkeo/nekonekostatus/releases/download/v0.1/neko-status_linux_${cpu} -O /usr/bin/neko-status 
